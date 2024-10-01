13b48f420c0b562de0535de349217aa17e0f0780
salmanals <salman.al.salem727@gmail.com>
2024-10-01 08:42:13 +0300

    Conditionals

---
+++ main.dart
@@ -0,0 +1,47 @@
+void main() {
+int mark = 77;
+if (mark >= 80 ){
+
+
+  print("A");
+}
+
+else if (mark >= 70 && mark < 80)
+ { print("B");
+}
+else if (mark >= 60 && mark < 70) {
+   print("C");
+
+
+}
+  
+else if (mark >= 50 && mark <60){
+  print("D");
+
+
+}
+ 
+
+else {print("F");} 
+
+int i = 50; 
+
+if (i % 3==0 && i%5==0){
+  print("FizzBuzz");
+}
+
+else if (i%3==0){
+  print("Fizz");
+} 
+else if (i % 5==0){
+  print("Buzz");
+}
+else {print("i");}
+
+
+
+
+
+
+
+}
